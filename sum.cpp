6bc2927e631404463bb8b6a3c07e1f4f097ad203
Abhimanyu890 <thakuranaabhimanyusingh@gmail.com>
2022-11-15 00:44:50 +0530

    done

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+int array[6]={1,2,3,4,10,11};
+
+
+int sum=0;
+for( int i=0;i<6;i++){
+   sum+=array[i];
+    
+}
+
+cout<<sum;
+ return 0;
+}
